6a7e1e53f35689dbe824fb90b1a851b895fcbf7c
Brianna O'Keefe <bri@songoda.com>
2018-12-29 14:05:19 -0500

    bit shifting.

--- src/main/java/com/songoda/ultimatestacker/events/InventoryListeners.java
+++ src/main/java/com/songoda/ultimatestacker/events/InventoryListeners.java
@@ -3,6 +3,7 @@ package com.songoda.ultimatestacker.events;
 import com.songoda.ultimatestacker.UltimateStacker;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -12,6 +13,34 @@ public class InventoryListeners implements Listener {
 
     public InventoryListeners(UltimateStacker instance) {
         this.instance = instance;
+    }
+
+    @EventHandler
+    public void onMove(InventoryMoveItemEvent event) {
+        ItemStack item = event.getItem();
+        if (new ItemStack(item.getType()).getMaxStackSize() == item.getMaxStackSize()) return;
+
+        instance.getStackingTask().setMax(item, 0, true);
+        int amt = item.getAmount();
+        int max = item.getMaxStackSize();
+
+        if (amt <= max) return;
+
+        item.setAmount(max);
+        amt = amt - max;
+
+        while (amt > max) {
+            ItemStack newItem = new ItemStack(item);
+            newItem.setAmount(max);
+
+            event.getDestination().addItem(newItem);
+            amt = amt - max;
+        }
+
+        ItemStack newItem = new ItemStack(item);
+        newItem.setAmount(amt);
+
+        event.getDestination().addItem(newItem);
     }
 
     @EventHandler

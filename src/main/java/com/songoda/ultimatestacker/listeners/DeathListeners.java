92a5d51846868b4f34bf162d84b1a919edce0894
Brianna <bri@songoda.com>
2019-07-05 13:47:29 -0400

    Attempt at better preserving custom drops.

--- src/main/java/com/songoda/ultimatestacker/listeners/DeathListeners.java
+++ src/main/java/com/songoda/ultimatestacker/listeners/DeathListeners.java
@@ -3,6 +3,7 @@ package com.songoda.ultimatestacker.listeners;
 import com.songoda.ultimatestacker.UltimateStacker;
 import com.songoda.ultimatestacker.utils.Methods;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 
@@ -16,7 +17,7 @@ public class DeathListeners implements Listener {
         this.instance = instance;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onEntityDeath(EntityDeathEvent event) {
         if (instance.getEntityStackManager().isStacked(event.getEntity()))
             instance.getEntityStackManager().getStack(event.getEntity())

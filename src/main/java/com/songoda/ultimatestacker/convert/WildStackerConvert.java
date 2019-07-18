904fdfd1dcd5d9930b9377ee27e19d94d9f6a5f3
Brianna <bri@songoda.com>
2019-07-18 15:43:55 -0400

    Fixed WildStacker conversion.

--- src/main/java/com/songoda/ultimatestacker/convert/WildStackerConvert.java
+++ src/main/java/com/songoda/ultimatestacker/convert/WildStackerConvert.java
@@ -2,12 +2,15 @@ package com.songoda.ultimatestacker.convert;
 
 import com.bgsoftware.wildstacker.WildStackerPlugin;
 import com.bgsoftware.wildstacker.api.WildStackerAPI;
-import com.bgsoftware.wildstacker.api.objects.StackedEntity;
 import com.bgsoftware.wildstacker.api.objects.StackedSpawner;
 import com.songoda.ultimatestacker.UltimateStacker;
 import com.songoda.ultimatestacker.entity.EntityStackManager;
 import com.songoda.ultimatestacker.spawner.SpawnerStack;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 
 public class WildStackerConvert implements Convert {
 
@@ -35,12 +38,17 @@ public class WildStackerConvert implements Convert {
     @Override
     public void convertEntities() {
         EntityStackManager entityStackManager = plugin.getEntityStackManager();
-        for (StackedEntity entity : WildStackerAPI.getWildStacker().getSystemManager().getStackedEntities()) {
-            if (!entityStackManager.isStacked(entity.getLivingEntity())) {
-                entityStackManager.addStack(entity.getLivingEntity(), entity.getStackAmount());
-                continue;
+        for (World world : Bukkit.getWorlds()) {
+            for (Entity entity : world.getEntities()) {
+                if (!(entity instanceof LivingEntity)) continue;
+                if (!entityStackManager.isStacked(entity)) {
+                    entityStackManager
+                            .addStack(entity, WildStackerAPI.getEntityAmount((LivingEntity)entity));
+                    continue;
+                }
+                entityStackManager
+                        .getStack(entity).setAmount(WildStackerAPI.getEntityAmount((LivingEntity)entity));
             }
-            entityStackManager.getStack(entity.getLivingEntity()).setAmount(entity.getStackAmount());
         }
 
     }
@@ -49,7 +57,9 @@ public class WildStackerConvert implements Convert {
     public void convertSpawners() {
         for (StackedSpawner spawner : WildStackerAPI.getWildStacker().getSystemManager().getStackedSpawners()) {
             SpawnerStack stack = plugin.getSpawnerStackManager().getSpawner(spawner.getLocation());
-            stack.setAmount(spawner.getStackAmount());
+
+            stack.setAmount(WildStackerAPI
+                    .getSpawnersAmount((CreatureSpawner) spawner.getLocation().getBlock().getState()));
             if (plugin.getHologram() != null)
                 plugin.getHologram().add(stack);
         }

8963ff707b5f7918d3a2f69194350a9bc7cd5531
jascotty2 <jascottytechie@gmail.com>
2019-09-03 16:35:52 -0500

    database refactor

--- src/main/java/com/songoda/ultimatestacker/database/migrations/_1_InitialMigration.java
+++ src/main/java/com/songoda/ultimatestacker/database/migrations/_1_InitialMigration.java
@@ -1,8 +1,8 @@
 package com.songoda.ultimatestacker.database.migrations;
 
 import com.songoda.core.database.DataMigration;
+import com.songoda.core.database.MySQLConnector;
 import com.songoda.ultimatestacker.UltimateStacker;
-import com.songoda.ultimatestacker.database.MySQLConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;

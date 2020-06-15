ee4cdfbdf53ccfb63468f0daef43b60665f251ad
Brianna <bri@songoda.com>
2020-06-15 17:25:19 -0500

    Strays are not in 1.8.8

--- src/main/java/com/songoda/ultimatestacker/lootables/LootablesManager.java
+++ src/main/java/com/songoda/ultimatestacker/lootables/LootablesManager.java
@@ -479,7 +479,7 @@ public class LootablesManager {
                 new LootBuilder()
                         .setChildDropCount(1)
                         .addOnlyDropFors(EntityType.SKELETON,
-                                EntityType.STRAY)
+                                ServerVersion.isServerVersionAtLeast(ServerVersion.V1_10) ? EntityType.STRAY : null)
                         .addChildLoot(new LootBuilder().setMaterial(CompatibleMaterial.MUSIC_DISC_11).build(),
                                 new LootBuilder().setMaterial(CompatibleMaterial.MUSIC_DISC_13).build(),
                                 new LootBuilder().setMaterial(CompatibleMaterial.MUSIC_DISC_BLOCKS).build(),
